9e5bce025185356e3d5114f2392290dd7e6b3c45
codingnaz <saimon_01@yahoo.com>
2019-04-04 13:02:33 -0400

    retroSpeed have value now

--- app/src/main/java/com/example/speedlimitretrofit/model/RetroSpeed.java
+++ app/src/main/java/com/example/speedlimitretrofit/model/RetroSpeed.java
@@ -2,4 +2,8 @@ package com.example.speedlimitretrofit.model;
 
 // TODO - Need more info about data response to implement model
 public class RetroSpeed {
+
+   private double current_lat, Current_lon;
+   private int speed_lim;
+
 }
