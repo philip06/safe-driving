dd7852cf8da863d0465f8cf609a5b03d3e39aef1
Michael-Roszel <roszel.michael@gmail.com>
2019-04-13 19:53:48 -0400

    starting to build navigation between different activities/screens

---
+++ app/src/main/java/display_Tolerance.java
@@ -0,0 +1,42 @@
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import com.example.speedlimitretrofit.R;
+
+public class display_Tolerance extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tolerance_input);
+    }
+
+    //set tolerance
+    protected void setTol()
+    {
+        //retrieve range of tolerance that was entered
+        EditText lowThresh = findViewById(R.id.lowThresh);
+        EditText highThresh = findViewById(R.id.highThresh);
+        String low = lowThresh.getText().toString();
+        String high = highThresh.getText().toString();
+
+        if(low.equals("") || high.equals(""))
+        {
+            //display message please put in valid range
+        }
+        int lowT = Integer.parseInt(low);
+        int highT = Integer.parseInt(high);
+
+        if(lowT < 0 || lowT > 70 || highT < 5 || highT > 100)
+        {
+            //display infeasible values
+        }
+        else if (lowT > highT || lowT == highT)
+        {
+            //display error
+        }
+
+        //set tolerance using lowT and highT
+    }
+}
