c642689a954019895cb11adf7f17501fd26dbe04
codingnaz <saimon_01@yahoo.com>
2019-04-28 15:05:28 -0400

    Merge branch 'master' of https://github.com/philip06/safe-driving into newmaster
    # Conflicts:
    #	app/src/main/java/com/example/speedlimitretrofit/api/model/querymodel/Around.java
    #	app/src/main/java/com/example/speedlimitretrofit/ui/MainActivity.java

---
+++ app/src/androidTest/java/com/example/speedlimitretrofit/api/model/overpassmodel/OverpassModelTest.java
@@ -0,0 +1,61 @@
+package com.example.speedlimitretrofit.api.model.overpassmodel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OverpassModelTest {
+    //OverpassModel testOPM= new OverpassModel();
+
+    @Test
+    public void getNodeList() {
+        OverpassModel testOPM= new OverpassModel();
+
+        //new Assert(testOPM.getNodeList(),null);
+    }
+
+    @Test
+    public void setNodeList() {
+    }
+
+    @Test
+    public void getWayList() {
+    }
+
+    @Test
+    public void setWayList() {
+    }
+
+    @Test
+    public void getNote() {
+    }
+
+    @Test
+    public void setNote() {
+    }
+
+    @Test
+    public void getMeta() {
+    }
+
+    @Test
+    public void setMeta() {
+    }
+
+    @Test
+    public void getVersion() {
+    }
+
+    @Test
+    public void setVersion() {
+    }
+
+    @Test
+    public void getGenerator() {
+    }
+
+    @Test
+    public void setGenerator() {
+    }
+}
