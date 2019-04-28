e11b2a1683d72ddb3d04e61e40548a081d1fd6dc
Michael-Roszel <roszel.michael@gmail.com>
2019-04-28 12:45:10 -0400

    Testing shared preference setting format

---
+++ app/src/main/java/com/example/speedlimitretrofit/ui/activities/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.example.speedlimitretrofit.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.example.speedlimitretrofit.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    public SettingsFragment(){
+
+    }
+    @Override public void onCreatePreferences(Bundle savedInstanceState, String rootKey)
+    {
+        setPreferencesFromResource(R.xml.settings_preferences,rootKey);
+    }
+}
