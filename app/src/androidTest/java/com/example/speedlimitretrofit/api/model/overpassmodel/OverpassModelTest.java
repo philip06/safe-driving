01597a36b23554211fc97f1cc2ad1f6faf11d3ba
codingnaz <saimon_01@yahoo.com>
2019-04-28 16:07:25 -0400

    Merge branch 'master' of https://github.com/philip06/safe-driving into newmaster
    # Conflicts:
    #	app/src/main/java/com/example/speedlimitretrofit/api/model/querymodel/Around.java
    #	app/src/main/java/com/example/speedlimitretrofit/ui/MainActivity.java

--- app/src/androidTest/java/com/example/speedlimitretrofit/api/model/overpassmodel/OverpassModelTest.java
+++ app/src/androidTest/java/com/example/speedlimitretrofit/api/model/overpassmodel/OverpassModelTest.java
@@ -3,6 +3,9 @@ package com.example.speedlimitretrofit.api.model.overpassmodel;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class OverpassModelTest {
@@ -13,49 +16,120 @@ public class OverpassModelTest {
         OverpassModel testOPM= new OverpassModel();
 
         //new Assert(testOPM.getNodeList(),null);
+        List<Node> list = new ArrayList();
+        Node temp_Node =new Node();
+        temp_Node.setId("0000001");
+        temp_Node.setLat("0000001");
+        temp_Node.setLon("0000001");
+        //list.add(temp_Node);
+        list.add(temp_Node);
+        testOPM.setNodeList(list);
+//        System.out.print(testOPM.getNodeList().size());
+       assertEquals(1,testOPM.getNodeList().size());
     }
 
     @Test
     public void setNodeList() {
+        OverpassModel testOPM= new OverpassModel();
+        List<Node> list = new ArrayList();
+        Node temp_Node =new Node();
+        temp_Node.setId("0000001");
+        temp_Node.setLat("0000001");
+        temp_Node.setLon("0000001");
+        //list.add(temp_Node);
+        list.add(temp_Node);
+        testOPM.setNodeList(list);
+//        System.out.print(testOPM.getNodeList().size());
+        assertEquals("0000001",testOPM.getNodeList().get(0).getId());
     }
+
 
     @Test
     public void getWayList() {
+        OverpassModel testOPM= new OverpassModel();
+        List<Way> list = new ArrayList();
+        Way temp_way =new Way();
+        temp_way.setId("01");
+        //list.add(temp_Node);
+        list.add(temp_way);
+        testOPM.setWayList(list);
+//        System.out.print(testOPM.getNodeList().size());
+        assertEquals("01",testOPM.getWayList().get(0).getId());
     }
 
     @Test
     public void setWayList() {
+        OverpassModel testOPM= new OverpassModel();
+        List<Way> list = new ArrayList();
+        //Way temp_way =new Way();
+        for(int i =0; i<5; i++){
+            Way temp_way =new Way();
+            temp_way.setId(String.valueOf(i));
+            list.add(temp_way);
+        }
+
+        testOPM.setWayList(list);
+//        System.out.print(testOPM.getNodeList().size());
+        assertEquals(5,testOPM.getWayList().size());
     }
 
     @Test
     public void getNote() {
+        OverpassModel testOPM= new OverpassModel();
+        testOPM.setNote("testing");
+        assertEquals("testing",testOPM.getNote());
     }
 
     @Test
     public void setNote() {
+        OverpassModel testOPM= new OverpassModel();
+        testOPM.setNote("testing");
+        assertEquals("testing",testOPM.getNote());
     }
 
     @Test
     public void getMeta() {
+        OverpassModel testOPM= new OverpassModel();
+        Meta temp_Meta = new Meta();
+        temp_Meta.setOsmBase("testing meta");
+        testOPM.setMeta(temp_Meta);
+        assertEquals("testing meta",testOPM.getMeta().getOsmBase());
     }
 
     @Test
     public void setMeta() {
+        OverpassModel testOPM= new OverpassModel();
+        Meta temp_Meta = new Meta();
+        temp_Meta.setOsmBase("testing meta");
+        testOPM.setMeta(temp_Meta);
+        assertEquals("testing meta",testOPM.getMeta().getOsmBase());
     }
 
     @Test
     public void getVersion() {
+        OverpassModel testOPM= new OverpassModel();
+        testOPM.setVersion("test");
+        assertEquals("test",testOPM.getVersion());
     }
 
     @Test
     public void setVersion() {
+        OverpassModel testOPM= new OverpassModel();
+        testOPM.setVersion("test");
+        assertEquals("test",testOPM.getVersion());
     }
 
     @Test
     public void getGenerator() {
+        OverpassModel testOPM= new OverpassModel();
+        testOPM.setGenerator("testing getGenerator");
+        assertEquals("testing getGenerator",testOPM.getGenerator());
     }
 
     @Test
     public void setGenerator() {
+        OverpassModel testOPM= new OverpassModel();
+        testOPM.setGenerator("testing getGenerator");
+        assertEquals("testing getGenerator",testOPM.getGenerator());
     }
 }
