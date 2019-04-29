1f7f9e6e75b85a829e99563fc8fa3ae7cd176659
codingnaz <saimon_01@yahoo.com>
2019-04-28 20:12:48 -0400

    #now shows the current speed, longgitude and latitude

---
+++ app/src/androidTest/java/com/example/speedlimitretrofit/api/model/querymodel/QueryModelTest.java
@@ -0,0 +1,21 @@
+package com.example.speedlimitretrofit.api.model.querymodel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QueryModelTest {
+
+    @Test
+    public void getQuery() {
+
+    }
+
+    @Test
+    public void getUnion() {
+    }
+
+    @Test
+    public void getPrint() {
+    }
+}
